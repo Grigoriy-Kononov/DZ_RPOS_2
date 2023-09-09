db59d9836d8973ebcc9413b2b9c23e2975841a81
Grigory <kononoff88@gmail.com>
2023-09-09 18:10:34 +0300

    2

---
+++ TabsAndTables/TabsAndTables/MyTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MyTabBarController.swift
+//  TabsAndTables
+//
+//  Created by Григорий Кононов on 08.08.2023.
+//
+
+import UIKit
+
+class MyTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
